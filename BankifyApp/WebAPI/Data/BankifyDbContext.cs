2187854660294e5ea032312f5caf4bad63e8acbe
justine-adan <justine.paul.adan.06@gmail.com>
2026-05-19 10:14:54 +0800

    added data model

---
+++ BankifyApp/WebAPI/Data/BankifyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class BankifyDbContext : DbContext
+    {
+        public BankifyDbContext(DbContextOptions<BankifyDbContext> options) : base(options)
+        {
+        }
+        public DbSet<BankifyUser> BankifyUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure BankifyUser entity
+            modelBuilder.Entity<BankifyUser>()
+                .HasKey(u => u.UserId);
+            modelBuilder.Entity<BankifyUser>()
+                .Property(u => u.UserRef)
+                .IsRequired()
+                .HasMaxLength(50);
+            modelBuilder.Entity<BankifyUser>()
+                .Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
--- BankifyApp/WebAPI/Program.cs
+++ BankifyApp/WebAPI/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Connection string for the database
+builder.Services.AddDbContext<BankifyDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
